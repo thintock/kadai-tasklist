538199d2658e050c2aa19511352e7fe23a9c2797
thintock <koki.git@alnaturia.com>
2021-02-11 12:58:04 +0000

    fix current user

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:show, :edit, :update]
   def index
     @tasks = Task.all
     if logged_in?
@@ -54,11 +55,17 @@ class TasksController < ApplicationController
 
 private
 
-
 # Strong Parameter
 
 def task_params
   params.require(:task).permit(:content, :status)
+end
+
+def correct_user
+  @task = current_user.tasks.find_by(id: params[:id])
+  unless @task
+    redirect_to root_url
+  end
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:index, :show, :new, :create]
-#  resources :users, only: [:new, :create]
   resources :tasks
 end
